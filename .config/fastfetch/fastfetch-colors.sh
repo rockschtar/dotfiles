252a7b3bf3d40df193bf078172cacd2a39e707e5
Stefan Helmer <sh@validio.de>
2024-04-20 02:21:33 +0200

    fixed macos logo color

--- .config/fastfetch/fastfetch-colors.sh
+++ .config/fastfetch/fastfetch-colors.sh
@@ -13,7 +13,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
             LOGO_COLOR="blue"
         fi    fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    LOGO_COLOR="green"
+    LOGO_COLOR="yellow"
 fi
 
 #fastfetch --load-config $HOME/.config/fastfetch/config.jsonc --logo $HOME/.config/fastfetch/cyberdyne.ascii --logo-type file --logo-color-1 $LOGO_COLOR
